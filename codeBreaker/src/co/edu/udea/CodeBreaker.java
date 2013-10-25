5b8897532597501a9b5e21dda9b25f44af294dc1
landrea-velez <landrea.velez@gmail.com>
2013-10-24 22:13:44 -0500

    Comentarios
    se quitaron los comentarios del código

--- codeBreaker/src/co/edu/udea/CodeBreaker.java
+++ codeBreaker/src/co/edu/udea/CodeBreaker.java
@@ -7,19 +7,7 @@ public class CodeBreaker {
 		String obtiene = "";
 		String numSecreto = "1234";
 
-		/*if (num.charAt(0)=='1'){
-			obtiene=obtiene+"X";
-		}
-		if (num.charAt(1)=='2'){
-			obtiene=obtiene+"X";
-		}
-		if (num.charAt(2)=='3'){
-			obtiene=obtiene+"X";
-		}
-		if (num.charAt(3)=='4'){
-			obtiene=obtiene+"X";
-		}*/
-		
+				
 		
 		for(int i=0;i<4;i++){
 			for (int j=0;j<4;j++){
@@ -34,14 +22,7 @@ public class CodeBreaker {
 			}
 		}
 		
-		/*for(int i=0;i<4;i++){
-			for (int j=0;j<4;j++){
-				if (i!=j){
-					if(numSecreto.charAt(i)==num.charAt(j))
-					obtiene=obtiene+"_";
-				}
-			}
-		}*/
+		
 		
 		return obtiene;
 	}
